569a6be9894f110d60ca325209401f1fc0df4a4a
Xinrui Ge <xinrui.ge24@gmail.com>
2024-03-23 11:48:02 -0400

    first commit

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,17 @@
+export default [ 
+  { "_id": "RS101", "name": "Rocket Propulsion", "number": "RS4550", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "/images/mellow-blue.jpeg"  },
+  { "_id": "RS102", "name": "Aerodynamics", "number": "RS4560", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "/images/pink.jpeg"  },
+  { "_id": "RS103", "name": "Spacecraft Design", "number": "RS4570", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "/images/orange.jpeg"  },
+
+  { "_id": "CS101", "name": "Web Development", "number": "CS4550", "startDate": "2024-01-05",
+    "endDate": "2024-04-29", "image": "/images/green.jpeg"  },
+  { "_id": "CS102", "name": "Theory of Computation", "number": "CS3800", "startDate": "2024-01-05",
+    "endDate": "2024-04-29", "image": "/images/purple.webp"  },
+  { "_id": "CS103", "name": "Algorithms & Data Structures", "number": "CS3000", "startDate": "2024-01-05",
+    "endDate": "2024-04-29", "image": "/images/gray.png"  },
+
+  { "_id": "PH101", "name": "Tech & Human Values", "number": "PH1145", "startDate": "2023-09-10",
+    "endDate": "2023-12-16", "image": "/images/mellow-blue.jpeg"  } ]
