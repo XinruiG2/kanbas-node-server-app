d6f8e0b64681c14e6a9478778f93c18605297f4a
Xinrui Ge <xinrui.ge24@gmail.com>
2024-04-04 15:43:44 -0400

    a6 server changes

--- App.js
+++ App.js
@@ -5,10 +5,35 @@ import cors from 'cors';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import QuizzesRoutes from './Kanbas/quizzes/routes.js';
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+  }));
+
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));  
+
 app.use(express.json());
+UserRoutes(app);
 QuizzesRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
